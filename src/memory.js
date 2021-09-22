94ac97e0cdef796486c2314c6b78a253932b6d57
kaleem99 <kaleem1999@outlook.com>
2021-09-22 21:12:30 +0200

    made changes

--- src/memory.js
+++ src/memory.js
@@ -1,69 +1,67 @@
-var array = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F"];
-
-var flipBack = "", firstvalue, secondValue, cardsHit = 0, cardChosen = 0;
-
-function shuffleArray() {
-  for (var i = 0; i < array.length; i++) {
-    var j = Math.floor(Math.random() * i);
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+const arrayImages = ["Paris.PNG", "Barcelona.PNG", "United.PNG", "Chelsea.PNG", "RealMadrid.PNG", "Bayern.PNG"];
+const newArray = [];
+let hasBeenClicked = [];
+let isCardsMatching = [];
+let newDivArray = [];
+let newDivNum = [];
+let clicks = 0;
+let counter = 0;
+function  shuffleArray(){
+  for(let i = 0; i < arrayImages.length; i++){
+    newArray.push(arrayImages[i], arrayImages[i])
   }
+  for(let i = 0; i < newArray.length; i++){
+    let j = Math.floor(Math.random() * i);
+    let temp = newArray[i];
+    newArray[i] = newArray[j];
+    newArray[j] = temp;
+  }
+  return newArray;
 }
+
 shuffleArray();
 
-function timer() {
-  setTimeout(checkforMatch, 500);
+function flip(num) {
+  if(clicks < 2 && (!newDivNum.includes(num)) && (!hasBeenClicked.includes(num))){
+    isCardsMatching.push(newArray[num]);
+    newDivNum.push(num)
+    $(`.grid-item${num} .back`).css('background-image', `url(../images/${newArray[num]})`);
+    $(`.grid-item${num}`).toggleClass('flipped');
+    clicks++;
+  }
+  console.log(counter, newArray.length)
+  if(clicks == 2){
+    if(isCardsMatching.length % 2 == 0 && isCardsMatching[0] != isCardsMatching[1]){
+      setTimeout(function(){
+        $(`.grid-item${newDivNum[0]}`).css('background-color', `#CCD2D9`);
+        $(`.grid-item${newDivNum[0]}`).toggleClass('flipped');
+        $(`.grid-item${newDivNum[1]}`).css('background-color', `#CCD2D9`);
+        $(`.grid-item${newDivNum[1]}`).toggleClass('flipped');
+        isCardsMatching = [];
+        clicks = 0;
+        newDivNum = [];
+      }, 2000)
+    }
+    else{
+      isCardsMatching = [];
+      clicks = 0;
+      let num1 = newDivNum[0];
+      let num2 = newDivNum[1];
+      hasBeenClicked.push(num1, num2)
+      newDivNum = [];
+      counter += 2;
+    }
+  }
+  checkIfBoardHasBeenFilled()
 }
 
-function onClick(index) {
-  if (cardChosen == 0) {
-    var allDivElements = document.getElementsByClassName("div-Elements");
-    firstvalue = index;
-    allDivElements[index].innerHTML = array[index];
-    cardChosen += 1;
-  } else {
-    var allDivElements = document.getElementsByClassName("div-Elements");
-    cardChosen += 2;
-    secondValue = index;
-    allDivElements[index].innerHTML = array[index];
-    timer();
-  }
-}
-
-function checkIfBoardIsFilled() {
-  if (cardsHit == array.length / 2) {
-    document.getElementById("H2").innerHTML = "welldone";
-    document.getElementById("colorBox").style.backgroundColor = "blue";
-    alert("Well done you completed the game press ok to play again");
-    setTimeout(function () {
-      location.reload();
-    }, 2000);
-  }
-}
-function timeOut(){
-   setTimeout(function () {
-    document.getElementById("colorBox").style.borderColor = "black";
-    }, 1000);
-}
-function checkforMatch() {
-  if (array[firstvalue] == array[secondValue]) {
-    var allDivElements = document.getElementsByClassName("div-Elements");
-    cardsHit++;
-    cardChosen = 0;
-    document.getElementById("H2").style.color = "blue";
-    document.getElementById("H2").innerHTML = "Match Found";
-    document.getElementById("colorBox").style.borderColor = "blue";
-    timeOut();
-  } else if (array[firstvalue] !== array[secondValue]) {
-    var allDivElements = document.getElementsByClassName("div-Elements");
-    allDivElements[firstvalue].innerHTML = flipBack;
-    allDivElements[secondValue].innerHTML = flipBack;
-    cardChosen = 0;
-    document.getElementById("H2").style.color = "red";
-    document.getElementById("H2").innerHTML = "Match Not Found Try Again";
-    document.getElementById("colorBox").style.borderColor = "red";
-    timeOut();
-  }
-  checkIfBoardIsFilled();
+function  checkIfBoardHasBeenFilled(){
+  setTimeout(function(){
+    if(counter == newArray.length){
+      alert("Well Done");
+      setTimeout(function(){
+        location.reload();
+      }, 1500)
+    }
+  }, 1000)
 }
